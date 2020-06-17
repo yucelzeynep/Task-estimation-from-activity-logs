229570389efba2cf6ba5ff24efe2a95484aba9cf
Zeynep Yucel <youcelzeynep@yahoo.com>
2020-06-17 15:03:16 +0900

    Meging many small file tools

--- file_tools.py
+++ file_tools.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 31 13:00:40 2019
+Created on Tue May 21 08:50:23 2019
 
-@author: florianpgn
+@author: zeynep
 """
 import numpy as np
+
+from importlib import reload
+from params import COL_NAMES
+
+import h5py
+
 
 def scanfMat(filename):
     """
@@ -17,4 +22,80 @@ def scanfMat(filename):
         mat.append(line.split())
     f.close()
     return np.asarray(mat, dtype=int)
+
+
+def readData(fname): 
+    
+    # Open file and read column names and data block 
+    f = open(fname) 
+    head = f.readline() # skip one line at the top
+    data_block = f.readlines()  # read all following lines
+    f.close() 
+    
+    # Create a data dictionary, containing 
+    # a list of values for each variable 
+    # Add an entry to the data dictionary for each column 
+    datalog = {} 
+    for col_name in COL_NAMES: 
+        datalog[col_name] = []
+    datalog['window_title'] = []
+   
+    # Loop through each value: append to each column 
+    line_count = 0
+    seperator = ';'
+    for line in data_block: 
         
+        items = line.split( seperator ) 
+
+        if len( items ) is not len(COL_NAMES):
+            print('Size mismatch: size is  {} instead of {} '.format(len( items ), len(COL_NAMES)))
+            
+        for n, item in enumerate(items):        
+            # We need to split the 'exe_name" column in two different columns 
+            if n == COL_NAMES.index('exe_name') :
+                datalog['exe_name'].append( item.split(':', 1)[0])
+                datalog['window_title'].append( item.split(':', 1)[1])
+                
+            else:
+                datalog[ COL_NAMES[n] ].append( item.rstrip('\n') )
+                
+
+        line_count += 1
+            
+    return datalog 
+        
+
+
+def decode(value):
+    if isinstance(value, np.ndarray):
+        return [decode(x) for x in value]
+    return value.decode('utf-8') if isinstance(value, bytes) else value
+
+def load(filename):
+    f = h5py.File(filename+'.dat', 'r')
+
+    """
+    If there is only one matrix we directly return it
+    """
+    if len(f.keys()) == 1 :
+        array_name = list(f.keys())[0]
+        arr = list(f[array_name])[0]
+        f.close()
+        decoded_list = decode(arr)
+        return np.array(decoded_list)
+    else:
+        data = []
+        # List all groups
+        for array_name in  f.keys():
+            # Get the data
+            decoded_list = [x.decode('utf-16') if isinstance(x, bytes) else x for x in list(f[array_name])[0]]
+            
+            #map(lambda x: x.decode('UTF-8'), list(f[array_name])[0])
+            data.append(decoded_list)
+        f.close()
+        return tuple(data)
+    
+def save(filename, dataset, datasetNames):
+    with h5py.File(filename+'.dat', 'w') as f:
+        for index, name in enumerate(datasetNames):
+            f.create_dataset(name, data=[dataset[index]])
